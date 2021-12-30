24fa2788508f7b938ae7db03f2c96d65520a0371
DGCK81LNN <triluolnn@163.com>
2021-12-30 22:08:41 +0800

    Add comment sections in blog posts

---
+++ _plugins/myfilters.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'digest/md5'
+
+module MyFilters
+  def soulblog_gravatar(email)
+    base = "https://gravatar.loli.net/avatar/"
+    if email && !email.empty?
+      hash = Digest::MD5.hexdigest(email.downcase)
+      "#{base}#{hash}?d=identicon"
+    else
+      "#{base}?f=y&d=mp"
+    end
+  end
+
+  def soulblog_emailsub(input)
+    input.gsub(/[@._-]/, {
+      "@" => " at ",
+      "." => " dot ",
+      "_" => " underscore ",
+      "-" => " dash ",
+    })
+  end
+end
+
+Liquid::Template.register_filter(MyFilters)
+
