9b020455caf527f389cccb4c74f4b0a12ebfaf90
DGCK81LNN <triluolnn@163.com>
2022-03-30 11:34:49 +0800

    Add ability to not show commenter email address

--- _plugins/myfilters.rb
+++ _plugins/myfilters.rb
@@ -4,14 +4,8 @@ require 'digest/md5'
 
 module SoulBlog
   module Filters
-    def soulblog_gravatar(email)
-      base = "https://gravatar.loli.net/avatar/"
-      if email && !email.empty?
-        hash = Digest::MD5.hexdigest(email.downcase)
-        "#{base}#{hash}?d=identicon"
-      else
-        "#{base}?f=y&d=mp"
-      end
+    def soulblog_md5(str)
+      Digest::MD5.hexdigest(str)
     end
 
     def soulblog_emailsub(input)
