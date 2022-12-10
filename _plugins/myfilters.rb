256ac9bac7f29dd7582421b91ed22edf83ec11e9
DGCK81LNN <triluolnn@163.com>
2022-12-11 00:46:09 +0800

    Switch to utteranc.es for comments

--- _plugins/myfilters.rb
+++ _plugins/myfilters.rb
@@ -1,23 +1,9 @@
 # frozen_string_literal: true
 
-require 'digest/md5'
 require 'base64'
 
 module SoulBlog
   module Filters
-    def soulblog_md5(str)
-      Digest::MD5.hexdigest(str)
-    end
-
-    def soulblog_emailsub(input)
-      input.gsub(/[@._-]/, {
-        "@" => " at ",
-        "." => " dot ",
-        "_" => " underscore ",
-        "-" => " dash ",
-      })
-    end
-
     def soulblog_base64(str)
       Base64.strict_encode64(str)
     end
