59e105bd186e015c5b6d57adfba1242f6cd618cc
DGCK81LNN <DGCK81LNN@users.noreply.github.com>
2024-12-29 15:27:48 +0000

    deploy: 3fd17cedaca938c20a6ecbead2b5995aa431c77c

---
+++ js/aqifeed.js
@@ -0,0 +1,1 @@
+function _aqiFeed(c,k,n){var L=document.createElement("script");k=_aqiFeed.k=_aqiFeed.k||(k?"&k="+k:""),_aqiFeed.c=c=c instanceof Array?c:[c],_aqiFeed.n=n=n||0;L.async=1,L.src="//feed.aqicn.org/feed/"+c[n].city+"/"+(c[n].lang||"")+"/feed.v1.js?n="+n+k,document.head.append(L)}
