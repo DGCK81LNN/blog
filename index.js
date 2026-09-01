9e5360016ad8c4d43fe17c35cd1bbcbbc3690ebd
DGCK81LNN <triluolnn@163.com>
2026-09-01 19:39:36 +0800

    update aqi city

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ setInterval(updateDateTime, 250)
 
 _aqiFeed({
   lang: "cn",
-  city: "china/chongzuo/shihuanbaojujiangzhoufenju",
+  city: "jiangsu/nanjing/xianlindaxuecheng",
   container: "linf-aqi",
   callback: function (response) {
     var el = document.createElement("div")
